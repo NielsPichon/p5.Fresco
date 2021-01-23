30cf685a43d4aa4d114bf21394bc39ad65482658
NielsP <niels.pichon@outlook.com>
2021-01-23 00:43:29 +0000

    Fiished implementing the cloud generator

---
+++ demos/theatre/cloudGen.js
@@ -0,0 +1,225 @@
+function createCloud() {
+    let vertices = [];
+  
+    // Initiate point
+    vertices.push(createPoint(0, cloudLevelHeight));
+    let vertex = createPoint(0, cloudLevelHeight);
+  
+    // Create left side of the cloud
+    let leftOrRight;
+    let maxRight = vertex.x;
+    let maxLeft = vertex.x;
+    let firstDirection;
+    for (let i = 0; i < cloudLevels; i++) {
+      // Choose left or right
+      let previousDir = leftOrRight;
+      leftOrRight = randomDirection();
+  
+      if (i > 0) {
+        // move up a half circle
+        if (previousDir == leftOrRight) {
+          sPath(1, previousDir, vertex, vertices);
+        }
+        else {
+          halfCircle(1, previousDir, vertex, vertices);
+        }
+      }
+      else {
+        firstDirection = leftOrRight;
+        vertices[0].x += leftOrRight * cloudLevelHeight * 0.5;
+      }
+  
+      // Move horizontally.
+      moveHorizontal(leftOrRight, vertex, vertices, 2 * cloudLevelHeight,
+          cloudMaxHorizontal - 2 * cloudLevelHeight);
+  
+      // If move right, check if new position is right of max right
+      // If so, store current position as max right
+      if (leftOrRight > 0 && vertex.x > maxRight) {
+        maxRight = vertex.x;
+      }
+      // If move left, check if new position is left of max left
+      // If so, store current position as max left
+      if (leftOrRight < 0 && vertex.x < maxLeft) {
+        maxLeft = vertex.x;
+      }
+    }
+  
+    // If last move is left, then go up
+    if (leftOrRight < 0) {
+      halfCircle(1, -1, vertex, vertices);
+    }
+  
+    // Minimum displacement to reach the right most position
+    let leftCap = maxRight + cloudMargin;
+    let horizontal = leftCap - vertex.x;
+  
+    // Move right 
+    moveHorizontal(1, vertex, vertices, horizontal, horizontal + cloudMaxHorizontal);
+    maxRight = vertex.x;
+    leftOrRight = 1;
+  
+    // Create right side of the cloud
+    for (let i = 0; i < cloudLevels; i++) {
+      
+      let previousDir = leftOrRight
+      // Choose direction. If already max Left, can only move right.
+      // Else choose randomly.
+      if (vertex.x - leftCap < cloudMargin) {
+        leftOrRight = 1
+      } else {
+        leftOrRight = randomDirection();
+      }
+  
+      // move down a half circle
+      if (previousDir == leftOrRight) {
+        sPath(-1, previousDir, vertex, vertices);
+      }
+      else {
+        halfCircle(-1, previousDir, vertex, vertices);
+      }
+      
+      // Move horizontally.
+      moveHorizontal(leftOrRight, vertex, vertices, 2 * cloudLevelHeight, cloudMaxHorizontal, leftCap);
+  
+      // If move right, check if new position is right of max right
+      // If so, store current position as max right
+      if (leftOrRight > 0 && vertex.x > maxRight) {
+        maxRight = vertex.x;
+      }
+      // If move left, check if new position is left of max left
+      // If so, store current position as max left
+      if (leftOrRight < 0 && vertex.x < maxLeft) {
+        maxLeft = vertex.x;
+      }
+    }
+  
+    // If last move is right, then down
+    if (leftOrRight > 0) {
+      // Move down 
+      halfCircle(-1, 1, vertex, vertices);
+      leftOrRight = 0; // prevent further offset
+     }
+  
+    // if higher than start point, move down
+    if (vertex.y > 0) {
+      // Move down
+      if (leftOrRight == -1) {
+        sPath(-1, leftOrRight, vertex, vertices);
+      }
+      else {
+        halfCircle(-1, leftOrRight, vertex, vertices);
+      }
+    }
+  
+    // move left to start point absiss
+    vertex.x = cloudLevelHeight * 0.5;
+    vertices.push(vertex.copy());
+    
+    //if lower than start point, move up
+    if (vertex.y < 0) {
+      botQuarterCircle(1, -1, vertex, vertices);
+      vertex.y = cloudLevelHeight * 0.5;
+      vertices.push(vertex.copy());
+      if (firstDirection == -1) {
+        topQuarterCircle(1, 1, vertex, vertices);
+      }
+      else {
+        topQuarterCircle(1, -1, vertex, vertices);
+      }
+    }
+    else {
+      if (firstDirection == -1) {
+        sPath(1, -1, vertex, vertices);
+      }
+      else {
+        halfCircle(1, -1, vertex, vertices);
+      }
+    }
+  
+    // center cloud and scale so that it that it always is of set width
+    let scaleRatio = cloudWidth / (maxRight - maxLeft);
+    let offset = (maxRight + maxLeft) / 2;
+    for (let i = 0; i < vertices.length; i++) {
+      vertices[i].x -= offset;
+      vertices[i].x *= scaleRatio;
+      vertices[i].y *= scaleRatio;
+    }
+  
+    // Create cloud shape
+    let cloud = new Fresco.Shape(vertices);
+    cloud.isPolygonal = true;
+  
+    return cloud;
+  }
+  
+  
+  function randomDirection(){
+    if (random() > 0.5) {
+      return 1;
+    }
+    return -1;
+  }
+  
+  
+  function halfCircle(verticalDirection, horizontalDirection,
+    vertex, vertices) {
+    botQuarterCircle(verticalDirection, horizontalDirection,
+        vertex, vertices);
+    topQuarterCircle(verticalDirection, horizontalDirection,
+        vertex, vertices);
+  }
+  
+  
+  function botQuarterCircle(verticalDirection, horizontalDirection,
+    vertex, vertices) {
+    let x0 = vertex.x;
+    let y0 = vertex.y + verticalDirection * cloudLevelHeight * 0.5;
+    quarterCircle(- Math.PI / 2, x0, y0, verticalDirection,
+        horizontalDirection, vertex, vertices);
+  }
+  
+  
+  function topQuarterCircle(verticalDirection, horizontalDirection,
+    vertex, vertices) {
+    let x0 = vertex.x - horizontalDirection * cloudLevelHeight * 0.5;
+    let y0 = vertex.y;
+    quarterCircle(0, x0, y0, verticalDirection,
+        horizontalDirection, vertex, vertices);
+  }
+
+  function quarterCircle(phase, x0, y0, verticalDirection,
+    horizontalDirection, vertex, vertices) {
+    for (let i = 0; i < cloudResolution; i++){
+        vertex.x = Math.cos(phase + (i + 1) / cloudResolution * Math.PI / 2) *
+          horizontalDirection * cloudLevelHeight * 0.5 + x0;
+        vertex.y = Math.sin(phase + (i + 1) / cloudResolution * Math.PI / 2) *
+          verticalDirection * cloudLevelHeight * 0.5 + y0;
+    
+        vertices.push(vertex.copy());
+      }    
+  }
+  
+  
+  function sPath(verticalDirection, horizontalDirection,
+    vertex, vertices) {
+    let x0 = vertex.x;
+    let y0 = vertex.y + verticalDirection * cloudLevelHeight * 0.5;
+    quarterCircle(-Math.PI / 2, x0, y0, verticalDirection, horizontalDirection, vertex, vertices);
+    x0 += horizontalDirection * cloudLevelHeight;
+    quarterCircle(0, x0, y0, verticalDirection, -horizontalDirection, vertex, vertices);
+  }
+  
+  
+  function moveHorizontal(direction, vertex, vertices, minDist, maxDist, leftCap=null) {
+    // The minimal displacement has to be at least equal to the minimum up displacement
+    let horizontal = random(minDist, maxDist);
+    vertex.add(createVector(horizontal * direction, 0));
+  
+    // make sure the point stays in bounds
+    if (leftCap) {
+      vertex.x = Math.max(vertex.x, leftCap);
+    }
+  
+    vertices.push(vertex.copy());
+  }
--- demos/theatre/theatre.js
+++ demos/theatre/theatre.js
@@ -2,16 +2,17 @@ const backgroundClr = '000';
 const numClouds = 1;
 const numWaves = 20;
 const cloudLevels = 3;
-const cloudUpMin = 30; 
-const cloudUpMax = 60;
+const cloudLevelHeight = 45; 
 const cloudMaxHorizontal = 200;
-const cloudMargin = 60;
+const cloudMargin = 90;
+const cloudWidth = 500;
+const cloudResolution = 8;
 
 let clouds = [];
 let waves = [];
 
 function setup() {
-  createCanvas(1000, 1000);
+  createCanvas(window.innerWidth, window.innerHeight);
   background(colorFromHex(backgroundClr));
   setSeed();
 
@@ -39,122 +40,6 @@ function draw() {
   noLoop();
 }
 
-function createCloud() {
-  let vertices = [];
-
-  // Initiate point
-  vertices.push(createPoint(0, 0));
-  let vertex = createPoint(0, 0);
-
-  // Create left side of the cloud
-  let leftOrRight;
-  let maxRight = vertex.x;
-  
-  for (let i = 0; i < cloudLevels; i++) {
-    // Move up 
-    // let up = random(cloudUpMin, cloudUpMax);
-    let up = (cloudUpMin + cloudUpMax) * 0.5;
-    vertex.add(createVector(0, up));
-    vertices.push(vertex.copy());
-    
-    // Move left or right
-    if (random() < 0.5) {
-      leftOrRight = -1;
-    } else {
-      leftOrRight = 1;
-    }
-    
-    // Move horizontally.
-    // The minimal displacement has to be at least equal to the minimum up displacement
-    let horizontal = random(up + cloudUpMax, cloudMaxHorizontal);
-    vertex.add(createVector(horizontal * leftOrRight, 0));
-    vertices.push(vertex.copy());
-
-    // If move right, check if new position is right of max right
-    // If so, store current position as max right
-    if (leftOrRight > 0) {
-      if (vertex.x > maxRight) {
-        maxRight = vertex.x;
-      }
-    }
-  }
-
-  // If last move is left, then go up
-  if (leftOrRight < 0) {
-     // Move up 
-    //  let up = random(cloudUpMin, cloudUpMax);
-     let up = (cloudUpMin + cloudUpMax) * 0.5;
-     vertex.add(createVector(0, up));
-     vertices.push(vertex.copy());   
-  }
-
-  // Minimum displacement to reach the right most position
-  let maxLeft = maxRight + cloudMargin;
-  let horizontal = maxLeft - vertex.x;
-
-  // Move right 
-  vertex.add(createVector(random(horizontal, horizontal + cloudMaxHorizontal), 0));
-  vertices.push(vertex.copy());
-
-  // Create right side of the cloud
-  for (let i = 0; i < cloudLevels; i++) {
-    // Move down
-    // let down = random(cloudUpMin, cloudUpMax);
-    let down = (cloudUpMin + cloudUpMax) * 0.5;
-    vertex.add(createVector(0, -down));
-    vertices.push(vertex.copy());
-    
-    // Move left or right
-    if (vertex.x - maxLeft < cloudMargin) {
-      leftOrRight = 1
-    } else {
-      if (random() < 0.5) {
-        leftOrRight = -1;
-      } else {
-        leftOrRight = 1;
-      }
-    }
-    
-    // Move horizontally.
-    // The minimal displacement has to be at least equal to the minimum up displacement
-    let horizontal = random(down + cloudUpMax, cloudMaxHorizontal);
-    vertex.add(createVector(horizontal * leftOrRight, 0));
-    vertex.x = Math.max(maxLeft, vertex.x);
-    vertices.push(vertex.copy());
-  }
-
-  // If last move is right, then down
-  if (leftOrRight > 0) {
-      // Move down 
-      // let down = random(cloudUpMin, cloudUpMax);
-      let down = (cloudUpMin + cloudUpMax) * 0.5;
-      vertex.add(createVector(0, -down));
-      vertices.push(vertex.copy());   
-   }
-
-  // if higher move down
-  if (vertex.y > 0) {
-    // Move down 
-    // let down = random(cloudUpMin, cloudUpMax);
-    let down = (cloudUpMin + cloudUpMax) * 0.5;
-    vertex.add(createVector(0, -down));
-    vertices.push(vertex.copy());   
-  }
-
-  // move left
-  vertices.push(createPoint(0, vertex.y));
-
-  //if lower move up
-  if (vertex.y < 0) {
-    vertices.push(createPoint(0, 0));
-  }
-
-  // Create cloud shape
-  let cloud = new Fresco.Shape(vertices);
-  // cloud.isPolygonal = true;
-
-  return cloud;
-}
 
 function createWave() {
 
