1c623e8fce264d7db5ea5bcea10d050b98dc35bc
NielsPichon <niels.pichon@outlook.com>
2021-08-22 12:50:22 +0200

    Some fixes to the font

---
+++ unitTests/fontTest/fontTest.js
@@ -0,0 +1,13 @@
+const backgroundClr = '000';
+
+function setup() {
+  createSVGCanvas(1000, 1000);
+  background(colorFromHex(backgroundClr));
+  setSeed();
+}
+
+// draw function which is automatically 
+// called in a loop
+function draw() {
+  Fresco.Futural.drawText('Hello World!', 18, createVector(0, 0), true);
+}
