21374c10d50e9f2d165951558ca2c0aff51dfcc1
NielsPichon <niels.pichon@outlook.com>
2023-06-11 12:20:26 +0200

    Added the ability to turn breathing into a commet

--- demos/breathing/breathing_base.js
+++ demos/breathing/breathing_base.js
@@ -1,15 +1,23 @@
-const backgroundClr = '00010f';
-const lineClr = 'ef476f';
-const lineAlpha = 255;
-const lineVerticesNum = 20;
-const lineNum = 200;
-const maxFactor = 1;
-const noiseFreq = 0.001;
-const noiseAmplitude = 25;
+const backgroundClr = '131619';
+const lineClr = 'FF6B6B';
+const lineAlpha = 128;
+const lineVerticesNum = 1000;
+const lineNum = 500;
+const maxFactor = 3.5;
+const noiseFreq = 0.002;
+const noiseAmplitude = 20;
+const maxNoiseFactor = 1;
 const displayCircle = false;
 const playBackSpeed = 2 * Math.PI / 360;
 const noiseModSpeed = 10;
-const startFactor = - Math.PI / 10;
+const startFactor =  0;
+const tailLengthFactor = 2;
+const globalRot = Math.PI / 8;
+// const globalRot = - 3 * Math.PI / 4;
+// const globalTranslationY = 350;
+const globalTranslationY = -200;
+const tailSharpness = 2;
+const tailSharpness2 = 6;
 
 const render = false;
 
@@ -20,10 +28,10 @@ let c;
 
 
 function setup() {
-  createCanvas(1440, 1440);
-  setSeed(151);
+  createA4RatioCanvas(1440);
+  setSeed(2847); // (freq (0.001), tailSharpness2 5) 7902, 4490 (20 points) , 1504 (50 points), 2267, 9092(1000 points), 2847 (1000 points, freq 0.002, tailSharpness 6), 8847 (globalRot = - 3 * Math.PI / 4  globalTranslationY = 300, noiseAmplitude 50, freq 0.002, 1000 points)
 
-  r = width / 2 - 100;
+  r = width / 2 - 300;
 
   c = new Fresco.Circle(12, r);
   c.color = colorFromHex(lineClr);
@@ -39,8 +47,8 @@ function getLineExtremities(index, lineNum) {
   v.mult(r);
   v = createPoint(v.x, v.y);
   return v;
-  
-} 
+
+}
 
 function draw() {
   background(colorFromHex(backgroundClr));
@@ -52,8 +60,27 @@ function draw() {
   }
 
  for (let i = 0; i < lineNum; i++) {
-  let a = getLineExtremities(i, lineNum); 
-  let b = getLineExtremities(i * map(Math.sin(factor), -1, 1, 0, maxFactor), lineNum);
+  let a = getLineExtremities(i, lineNum);
+  let b = getLineExtremities(
+    i * map(Math.sin(factor), -1, 1, 0, maxFactor), lineNum);
+  let extra = b.copy().normalize();
+  let r = width / 2 - 300;
+  let angle = - 3 * PI / 4;
+
+  if (random() <.5) {
+    let hole = max(0, extra.x * cos(angle) + extra.y * sin(angle))
+        * (1 - 0 * noise(
+          a.x * noiseFreq,
+          a.y * noiseFreq,
+          frameCount * noiseFreq * noiseModSpeed
+    ));
+
+    for (let k = 0; k < tailSharpness; k++) {
+      hole = invertSmoothstep(hole);
+    }
+
+    b.add(extra.mult(r * tailLengthFactor * pow(hole, tailSharpness2)));
+  }
 
   // create new subdivided line
   let l = new Fresco.Line(a, b, lineVerticesNum);
@@ -66,22 +93,37 @@ function draw() {
 
       // Create noise
       let noiseVect = noiseVector(
-        normalizedRidgedNoise, 
+        normalizedRidgedNoise,
         l.vertices[j].x * noiseFreq,
-        l.vertices[j].y * noiseFreq, 
+        l.vertices[j].y * noiseFreq,
         frameCount * noiseFreq * noiseModSpeed
       );
-      noiseVect.mult(noiseAmplitude);
+
+      let amplitude =lerp(
+        noiseAmplitude,
+        noiseAmplitude * maxNoiseFactor,
+        invertSmoothstep(constrain(0, 20, abs(i - 313)) / 313)
+          * smoothstep(j / l.vertices.length)
+      );
+      noiseVect.mult(amplitude);
       // Add noise = displace vertex based on noise
       l.vertices[j].add(noiseVect);
       // Check if vertex is still in the circle
-      if (l.vertices[j].magSq() > r * r) {
-        // If out, bring back on circle
-        l.vertices[j] = l.vertices[j].normalize().mult(r);
-      }
+      // if (l.vertices[j].magSq() > r * r) {
+      //   // If out, bring back on circle
+      //   l.vertices[j] = l.vertices[j].normalize().mult(r);
+      // }
     }
 
+    l.vertices.forEach(v => {
+      let x = v.x; y = v.y;
+      v.y = -x;
+      v.x = -y;
+    });
+
     // draw
+    l.setRotation(globalRot);
+    l.setPosition(createPoint(0, globalTranslationY));
     l.draw();
   }
   noLoop()
